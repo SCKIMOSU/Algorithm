5135f308163131c81fc311caf73e9dd8b091b45f
sckimosu <sckim7@kookmin.ac.kr>
2017-05-18 13:17:56 +0900

    Add triangular.cpp

---
+++ triangular.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int n;
+
+int solve()
+{
+    int cnt=0;
+    scanf("%d", &n);
+
+    int count=0;
+
+    for (int a=1; a<=n; a++)
+        for (int b=a; b<=n; b++)
+            for (int c=b; c<=n; c++)
+            {
+                if(count%5==0) puts("");
+                    count++;
+                printf("[%d %d %d]\t", a,b, c);
+
+
+
+            }
+
+
+    for(int a=1; a<=n; a++)
+        for(int b=a; b<=n; b++)
+            for(int c=b; c<=n; c++)
+                if(a+b+c==n && a+b>c)
+                    cnt++;
+    return cnt;
+
+
+}
+
+int main()
+{
+    printf("\n answer(cnt)= %d\n", solve());
+}
+
