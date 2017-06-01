1eb6eb2d014601c3d8c4ae0c0d4e1d9fb635d643
sckimosu <sckim7@kookmin.ac.kr>
2017-06-01 23:03:06 +0900

    Add triangular.cpp

--- triangular.cpp
+++ triangular.cpp
@@ -21,12 +21,18 @@ int solve()
 
             }
 
+   puts("");
+   puts("");
 
     for(int a=1; a<=n; a++)
         for(int b=a; b<=n; b++)
             for(int c=b; c<=n; c++)
                 if(a+b+c==n && a+b>c)
-                    cnt++;
+                    {
+
+                        cnt++;
+                        printf(" result=[%d %d %d]\n", a,b, c);
+                    }
     return cnt;
 
 
